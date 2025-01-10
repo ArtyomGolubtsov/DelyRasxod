5538b726b06d869c2d156cc3256744b29fea16a4
serdyukoff <61atigr61@gmail.com>
2025-01-10 22:50:47 +0300

    Первое обновление 🥳

--- app/src/main/java/com/example/bankapp/SignUpActivity.kt
+++ app/src/main/java/com/example/bankapp/SignUpActivity.kt
@@ -1,35 +1,20 @@
 package com.example.bankapp
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.AppCompatButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class GreatingActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_greating)
+        setContentView(R.layout.activity_sign_up)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val btnSignUp = findViewById<AppCompatButton>(R.id.btnSignUp)
-        val btnSignIn = findViewById<AppCompatButton>(R.id.btnSignIn)
-
-        btnSignUp.setOnClickListener {
-            val intent = Intent(this@GreatingActivity, SignUpActivity::class.java)
-            startActivity(intent)
-        }
-
-        btnSignIn.setOnClickListener {
-            val intent = Intent(this@GreatingActivity, SignInActivity::class.java)
-            startActivity(intent)
         }
     }
 }
